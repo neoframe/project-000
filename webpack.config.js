67847ceb0e96eb41cb97968cc7642f0adf0715c8
dackmin <ugo@poool.fr>
2021-10-22 15:38:17 +0200

    chore: init project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    open: true,
+    hot: true,
+    host: 'localhost',
+    port: 29997,
+    historyApiFallback: true,
+  },
+  mode: 'development',
+  target: 'web',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        caseSensitive: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        collapseBooleanAttributes: true,
+        removeCommentsFromCDATA: true,
+        minifyJS: true,
+      },
+    }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.[fullhash].js',
+    sourceMapFilename: 'bundle.[fullhash].js.map',
+    publicPath: '/',
+  },
+};
